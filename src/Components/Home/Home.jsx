dbe3f10727f8987eaf27a1a2566eadf0839f49b3
MD Taukir Alam <taukir007alam@gmail.com>
2023-07-31 01:46:19 +0600

    version control purpose

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,7 +5,17 @@ const Home = () => {
   return (
     <div>
       <Header></Header>
-      <div>This is Home</div>
+      <div>
+        <h1 className="text-7xl">
+          One Step Closer To Your <br></br>
+          <span className="text-green-500">Dream Body</span>
+        </h1>
+        <p className="text-lg">
+          Transform Your Body, Elevate Your Mind. Get Fit, Stay Strong, Be
+          Unstoppable. Embrace the Gym, Unleash Your Power.
+        </p>
+        <button>Get Started</button>
+      </div>
       <Outlet></Outlet>
     </div>
   );
